44b448ab97ec2e51eb21fba521fa31788fc56923
César Carchi <cacarchi@outlook.com>
2022-04-03 21:24:15 -0500

    Se modifica Post.jsx y PostList.jsx para agregar más entradas a los posts

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,15 +1,15 @@
-function Post ({name,title,description,img}) {
+function Post ({date,likes,name,title,description,img,comments}) {
     return (
         <div className="card mb-3">
             <img className="card-img-top" src={img} alt="..." />
             <div className="card-body">
                 <div className="d-flex justify-content-between align-items-center no-margin no-padding">
-                    <p className="time">3min ago</p>
+                    <p className="time">{date}</p>
                     <a href="..." className="like btn btn-danger">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-heart-fill" viewBox="0 0 16 16">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-heart-fill mr-2" viewBox="0 0 16 16">
                             <path fill-rule="evenodd" d="M8 1.314C12.438-3.248 23.534 4.735 8 15-7.534 4.736 3.562-3.248 8 1.314z"/>
                         </svg>
-                        43k
+                        {likes}
                     </a>
                 </div>
                 <h3>{name}</h3>
@@ -18,7 +18,7 @@ function Post ({name,title,description,img}) {
                 <div className="comments">
                     <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-chat-right" viewBox="0 0 16 16">
                         <path d="M2 1a1 1 0 0 0-1 1v8a1 1 0 0 0 1 1h9.586a2 2 0 0 1 1.414.586l2 2V2a1 1 0 0 0-1-1H2zm12-1a2 2 0 0 1 2 2v12.793a.5.5 0 0 1-.854.353l-2.853-2.853a1 1 0 0 0-.707-.293H2a2 2 0 0 1-2-2V2a2 2 0 0 1 2-2h12z"/>
-                    </svg> Comments (15)
+                    </svg> Comments ({comments})
                 </div>
             </div>
         </div>
--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -4,13 +4,13 @@ function PostList () {
     return (
         <div className="cards row">
             <div className="col-xl-4 col-lg-4 col-md-6 col-sm-6 col-xs-12">
-                <Post name="@César" title="Card TreePic 1" description="This is a description of the card. TreePics 1" img="https://source.unsplash.com/random/800x800" />
+                <Post date="2 hour" likes=" 5m" name="@César" title="Card TreePic 1" description="This is a description of the card. TreePics 1" img="https://source.unsplash.com/random/800x800" comments="200" />
             </div>
             <div className="col-xl-4 col-lg-4 col-md-6 col-sm-6 col-xs-12">
-                <Post name="@Raúl" title="Card TreePic 2" description="This is a description of the card. TreePics 2" img="https://source.unsplash.com/random/800x800" />
+                <Post date="45 min" likes=" 10k" name="@Raúl" title="Card TreePic 2" description="This is a description of the card. TreePics 2" img="https://source.unsplash.com/random/800x800" comments="5"/>
             </div>
             <div className="col-xl-4 col-lg-4 col-md-6 col-sm-6 col-xs-12">
-                <Post name="@Jefferson" title="Card TreePic 3" description="This is a description of the card. TreePics 3" img="https://source.unsplash.com/random/800x800" />
+                <Post date="1 day" likes=" 500" name="@Jefferson" title="Card TreePic 3" description="This is a description of the card. TreePics 3" img="https://source.unsplash.com/random/800x800" comments="30"/>
             </div>
         </div>
     );
